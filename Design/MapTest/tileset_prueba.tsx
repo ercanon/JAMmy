a6491f0bc8e3265089b00a90954765c07e92f5f2
Turmo11 <45201984+Turmo11@users.noreply.github.com>
2023-06-19 23:06:32 +0200

    Tiled files

---
+++ Design/MapTest/tileset_prueba.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="tileset_prueba" tilewidth="16" tileheight="16" tilecount="1975" columns="25">
+ <image source="sv_tileset.png" width="400" height="1264"/>
+</tileset>
