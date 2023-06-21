c0d438323ef1cf89ad6f7d8dafcc090652733de4
paumotta <61042875+paumotta@users.noreply.github.com>
2023-06-21 05:28:37 +0200

    Ancient map finished

--- Design/Map/Tilesets/ancient.tsx
+++ Design/Map/Tilesets/ancient.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="ancient" tilewidth="32" tileheight="32" tilecount="104" columns="13">
+<tileset version="1.8" tiledversion="1.8.5" name="ancient" tilewidth="32" tileheight="32" tilecount="104" columns="13">
  <image source="../Textures/Ancient_Tileset.png" width="416" height="256"/>
 </tileset>
