6f070989f9479a575a8c4312f98e1d2eb67a1fb2
Turmo11 <45201984+Turmo11@users.noreply.github.com>
2023-06-21 03:57:05 +0200

    Some changes for the level building

---
+++ Design/Map/Tilesets/reference.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="reference" tilewidth="1056" tileheight="1152" tilecount="1" columns="1">
+ <image source="../Textures/test_map-export.png" width="1056" height="1152"/>
+</tileset>
